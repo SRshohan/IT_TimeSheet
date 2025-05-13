ee728da46b1424c518373a027fb7693e51675df4
SRshohan <srshohan02@gmail.com>
2025-05-13 16:03:45 -0400

    Added extract time function

--- backend/extract_time.py
+++ backend/extract_time.py
@@ -1,2 +1,37 @@
 import time
+from setup import setup_driver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+
+# Collect data from the website
+data = []
+
+# Setup driver
+driver = setup_driver()
+# WebDriverWait
+wait = WebDriverWait(driver, 10)
+
+driver.get("https://www.opentimeclock.com/free.html?page=2004&v=1665245702874")
+
+wait.until(EC.element_to_be_clickable((By.ID, "txtUser"))).send_keys("srahman06")
+
+wait.until(EC.element_to_be_clickable((By.ID, "txtPassword"))).send_keys("sohanur")
+
+wait.until(EC.element_to_be_clickable((By.ID, "btnLogin"))).click()
+
+# Wait for the table or a known element to be present
+wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "div[style='text-align:center;width:90px;float:left;']")))
+
+# Extract all date cells
+date_divs = driver.find_elements(By.CSS_SELECTOR, "div[style='text-align:center;width:90px;float:left;']")
+for div in date_divs:
+    print(div.text)
+
+# If you want to extract entire rows, you may need to adjust the selector
+
+driver.quit()
+
+
+
 
--- backend/setup.py
+++ backend/setup.py
@@ -4,20 +4,29 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import os
 import time
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from dotenv import load_dotenv
 
 load_dotenv()
 
-path = "../chromedriver-mac-arm64/chromedriver"
 
-if not os.path.exists(path):
-    raise Exception("Path doesn't exist!")
+def setup_driver():
 
-service = Service(path)
-options = Options()
-# options.add_argument("--headless=new")  # Uncomment if you want background mode
+    path = "../chromedriver-mac-arm64/chromedriver"
 
-driver = webdriver.Chrome(service=service)
+    if not os.path.exists(path):
+        raise Exception("Path doesn't exist!")
+
+    service = Service(path)
+    options = Options()
+    # options.add_argument("--headless=new")  # Uncomment if you want background mode
+
+    driver = webdriver.Chrome(service=service)
+
+    return driver
+
+driver = setup_driver()
 
 driver.get("https://selfservice.manhattan.edu/")
 
@@ -31,9 +40,9 @@ password = os.getenv("PASSWORD")
 driver.find_element(By.ID, "username").send_keys(username)
 driver.find_element(By.ID, "password").send_keys(password)
 driver.find_element(By.NAME, "_eventId_proceed").click()
-time.sleep(10)
 
-duo_header = driver.find_element(By.ID, "header-text")
+wait = WebDriverWait(driver, 20)
+duo_header = wait.until(EC.presence_of_element_located((By.ID, "header-text")))
 
 if duo_header.text == "Enter code in Duo Mobile":
     print(duo_header.text)
@@ -44,31 +53,21 @@ if duo_header.text == "Enter code in Duo Mobile":
     trust_screen = driver.find_element(By.ID, "trust-this-browser-label")
     print(trust_screen.text)
     if trust_screen.text == "Is this your device?":
-        driver.find_element(By.ID, "dont-trust-browser-button").click()
+        driver.find_element(By.ID, "trust-browser-button").click()
         time.sleep(5)
-        click_banner_menu = driver.find_element(By.ID, "bannerMenu")
-        print(click_banner_menu.text)
-        click_banner_menu.click()
-        time.sleep(5)
-        driver.find_element(By.CLASS_NAME, "menu-item menu-common").click()
-        driver.find_element(By.CLASS_NAME, "menu-common").click()
-        driver.find_element(By.ID, "menuList").click()
-        driver.find_element(By.CLASS_NAME, "menu-text menu-common").click()
+        driver.get("https://banner-ssb-prod.manhattan.edu/PROD/bwpktais.P_SelectTimeSheetRoll")
+        time.sleep(10)
+        # click_banner_menu.click()
+        # print("Clicked banner menu")
+        # time.sleep(5)
+        # driver.find_element(By.ID, "menuList").click()
+        # print("Clicked Banner common Menu!")
+        # driver.find_element(By.CLASS_NAME, "menu-common").click()
+        # driver.find_element(By.ID, "menuList").click()
+        # driver.find_element(By.CLASS_NAME, "menu-text menu-common").click()
     else:
         print("Duo is not working")
 else:
     print(duo_header.text)
-
-
-
-
-
-
-
-
-
-
-
-
 
 
